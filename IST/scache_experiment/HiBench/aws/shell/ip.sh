d789d7845c31dd861a62b7048b95c3c330c0a2d3
wuchunghsuan <wuchunghsuan@gmail.com>
2018-08-01 09:12:47 +0000

    ADD aws.

---
+++ IST/scache_experiment/HiBench/aws/shell/ip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+END="\033[0m"
+
+IPS=()
+PORT=1022
+for ((i=2;i<5;i++)); do
+        #if [ $i -lt 10 ]; then num=0$i; else num=$i; fi
+        #ip=127.17.0.$num
+        IP=192.168.2.1$i
+	IPS=( ${IPS[@]} $IP )
+done
+
+echo " IPS:"
+length=${#IPS[@]}
+for ((i=0; i<length; i++))
+do
+	echo -e "${GREEN}	${IPS[$i]}${END}"
+done
---
+++ IST/scache_experiment/HiBench/aws/shell/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. ./functions.sh
+
+#export -f scp_function expect_function
+#parallel scp_function ::: "${IPS[@]}" ::: "../tar/hadoop-2.7.5.tar" ::: "/"
+
+#scp_hadoop
+tar_hadoop
+
+
