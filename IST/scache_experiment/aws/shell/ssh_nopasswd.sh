f9ab03111c4e26657f7daf20a9bc80c5c4393735
wuchunghsuan <wuchunghsuan@gmail.com>
2018-08-02 08:28:03 +0000

    Add ssh_nopasswd.sh

---
+++ IST/scache_experiment/aws/shell/ssh_nopasswd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. ./functions.sh
+
+ssh-keygen -t rsa
+
+function ssh_copy_function() {
+        IP=$1
+        PORT=$2
+        FROM=$3
+        PASSWD=$4
+        #echo -e " SCP IP:${GREEN}$IP${END} PORT:${GREEN}$PORT${END} FROM:${BLUE}$FROM${END} TO:${BLUE}$TO${END}"
+        CMD="ssh-copy-id -i $FROM -p $PORT $IP"
+
+        expect_function "$CMD" $PASSWD
+}
+
+FROM=~/.ssh/id_rsa.pub
+export -f ssh_copy_function expect_function
+parallel ssh_copy_function ::: ${IPS[@]} ::: $PORT  ::: $FROM ::: $PASSWD
