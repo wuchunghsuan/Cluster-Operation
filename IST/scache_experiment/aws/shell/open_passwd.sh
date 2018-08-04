eb4851c5fcec74991c2cde3548dcfd7ccce1c6c0
Wu, ChungHsuan <wuchunghsuan@gmail.com>
2018-08-04 18:30:29 +0800

    Update open_passwd.sh

--- IST/scache_experiment/aws/shell/open_passwd.sh
+++ IST/scache_experiment/aws/shell/open_passwd.sh
@@ -7,10 +7,10 @@ FILE=/etc/ssh/sshd_config
 
 CMD1="echo -e \\\"1234qwer\\\n1234qwer\\\" | sudo passwd"
 
-CMD2="chmod +x $FILE && sed -i \\\"/$KEY1/{s/$KEY1.*/$KEY1 yes/}\\\" $FILE && sed -i \\\"/$KEY2/{s/$KEY2.*/$KEY2 yes/}\\\" $FILE && systemctl restart sshd"
+CMD2="sudo chmod +x $FILE && sudo sed -i \\\"/$KEY1/{s/$KEY1.*/$KEY1 yes/}\\\" $FILE && sudo sed -i \\\"/$KEY2/{s/$KEY2.*/$KEY2 yes/}\\\" $FILE && sudo systemctl restart sshd"
 
 export -f ssh_pem_function expect_function
-#parallel ssh_pem_function ::: "${IPS[@]}" ::: $PORT ::: "$CMD1" ::: ../pem/renrui_california.pem
+parallel ssh_pem_function ::: "${IPS[@]}" ::: $PORT ::: "$CMD1" ::: ../pem/renrui_california.pem
 
-export -f ssh_function
-parallel ssh_function ::: "${IPS[@]}" ::: $PORT ::: "$CMD2" ::: $PASSWD
+export -f ssh_pem_function
+parallel ssh_pem_function ::: "${IPS[@]}" ::: $PORT ::: "$CMD2" ::: ../pem/renrui_california.pem
