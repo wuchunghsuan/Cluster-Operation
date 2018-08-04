fea2e7f3c8e3844fb73751a47299fc67fb3ddb48
wuchunghsuan <wuchunghsuan@gmail.com>
2018-08-04 09:34:02 +0000

    Update open_passwd.sh

--- IST/scache_experiment/aws/shell/ip.sh
+++ IST/scache_experiment/aws/shell/ip.sh
@@ -6,13 +6,13 @@ BLUE="\033[34m"
 END="\033[0m"
 
 IPS=()
-PORT=1022
-for ((i=1;i<5;i++)); do
-        #if [ $i -lt 10 ]; then num=0$i; else num=$i; fi
-        #ip=127.17.0.$num
-        IP=192.168.2.1$i
-	IPS=( ${IPS[@]} $IP )
-done
+PORT=22
+#for ((i=1;i<5;i++)); do
+#        #if [ $i -lt 10 ]; then num=0$i; else num=$i; fi
+#        #ip=127.17.0.$num
+#        IP=192.168.2.1$i
+#	IPS=( ${IPS[@]} $IP )
+#done
 MASTER=${IPS[0]}
 
 echo " IPS:"
---
+++ IST/scache_experiment/aws/shell/open_passwd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. ./functions.sh
+
+KEY1=PermitRootLogin
+KEY2=PasswordAuthentication
+FILE=/etc/ssh/sshd_config
+
+CMD1="echo -e \"1234qwer\n1234qwer\" | sudo passwd"
+
+CMD2="chmod +x $FILE && sed -i \"/$KEY1/{s/$KEY1.*/$KEY1 yes/}\" $FILE && sed -i \"/$KEY2/{s/$KEY2.*/$KEY2 yes/}\" $FILE && systemctl restart sshd"
+
+export -f ssh_pem_function
+parallel ssh_pem_function ::: "${IPS[@]}" ::: $PORT ::: $CMD1 ::: ../pem/renrui.pem
+
+export -f ssh_function
+parallel ssh_function ::: "${IPS[@]}" ::: $PORT ::: $CMD2 ::: $PASSWD
