4444dcc08486a9846ae00ea7062f89c4dbb6d6eb
Wu, ChungHsuan <wuchunghsuan@gmail.com>
2018-08-06 12:27:03 +0800

    Update full_run.sh

--- IST/scache_experiment/HiBench/terasort/full_run.sh
+++ IST/scache_experiment/HiBench/terasort/full_run.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-./conf_slowstart.sh $1
-
-REPORTDIR="/root/report-ST$1"
+REPORTDIR=$1
 mkdir $REPORTDIR 
 
+./conf_slowstart.sh $2
 ./run.sh 100 400  200 $REPORTDIR
+./conf_slowstart.sh $3
 ./run.sh 100 800  200 $REPORTDIR
+./conf_slowstart.sh $4
 ./run.sh 100 1600 200 $REPORTDIR
 
+./conf_slowstart.sh $2
 ./run.sh 200 400  200 $REPORTDIR
+./conf_slowstart.sh $3
 ./run.sh 200 800  200 $REPORTDIR
+./conf_slowstart.sh $4
 ./run.sh 200 1600 200 $REPORTDIR
 
+#./conf_slowstart.sh $2
 #./run.sh 400 400  200 $REPORTDIR
+#./conf_slowstart.sh $3
 #./run.sh 400 800  200 $REPORTDIR
+#./conf_slowstart.sh $4
 #./run.sh 400 1600 200 $REPORTDIR
-#
+
+#./conf_slowstart.sh $2
 #./run.sh 800 400  200 $REPORTDIR
+#./conf_slowstart.sh $3
 #./run.sh 800 800  200 $REPORTDIR
+#./conf_slowstart.sh $4
 #./run.sh 800 1600 200 $REPORTDIR
